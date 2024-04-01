33899ec17ac594b06b4da689737a350306a776c3
alkaz07 <alkaz07@yandex.ru>
2024-04-01 12:17:48 +0300

    создал класс Точка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point p = new Point(6, 8);
+    }
+}
+
+class Point{
+    double x,y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 }
